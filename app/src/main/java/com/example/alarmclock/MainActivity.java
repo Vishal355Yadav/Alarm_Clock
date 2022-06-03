7b74d1c99b14867d3249930755940ce18861dcf7
Vishal355Yadav <201b355@juetguna.in>
2022-06-03 17:12:19 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/alarmclock/MainActivity.java
@@ -0,0 +1,191 @@
+package com.example.alarmclock;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.alarmclock.databinding.ActivityMainBinding;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
+
+import java.util.Calendar;
+
+
+public class MainActivity extends AppCompatActivity {
+    //menu formation code
+         @Override
+         public boolean onOptionsItemSelected(@NonNull MenuItem item){
+             super.onOptionsItemSelected(item);
+           switch(item.getItemId()){
+            case R.id.current_time:
+                //some code to be written
+
+                return true;
+            case R.id.stopwatch:
+                //some code to be written
+                return true;
+            case R.id.about_us:
+                //some code needed
+                Intent intent= new Intent(this,AboutUsActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return true;
+        }
+    }
+
+        @Override
+         public boolean onCreateOptionsMenu(Menu menu){
+             MenuInflater inflater=getMenuInflater();
+             inflater.inflate(R.menu.alarm_menu,menu);
+             return super.onCreateOptionsMenu(menu);
+//
+    }
+
+    //
+    private ActivityMainBinding binding;
+    private MaterialTimePicker picker;
+    private Calendar calendar;
+    private AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        createNotificationChannel();
+
+        binding.selectTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showTimePicker();
+
+            }
+        });
+
+        binding.setalarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                setAlarm();
+
+            }
+        });
+
+        binding.cancelalr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                cancelAlarm();
+
+            }
+        });
+
+    }
+
+    private void cancelAlarm() {
+
+        Intent intent = new Intent(this,AlarmReciver.class);
+
+        pendingIntent = PendingIntent.getBroadcast(this,0,intent,0);
+
+        if (alarmManager == null){
+
+            alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        }
+
+        alarmManager.cancel(pendingIntent);
+        Toast.makeText(this, "Alarm Cancelled", Toast.LENGTH_SHORT).show();
+    }
+
+    private void setAlarm() {
+
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent intent = new Intent(this,AlarmReciver.class);
+
+        pendingIntent = PendingIntent.getBroadcast(this,0,intent,0);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY,pendingIntent);
+
+        Toast.makeText(this, "Alarm set Successfully", Toast.LENGTH_SHORT).show();
+
+
+
+    }
+
+    private void showTimePicker() {
+
+        picker = new MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setHour(12)
+                .setMinute(0)
+                .setTitleText("Select Alarm Time")
+                .build();
+
+        picker.show(getSupportFragmentManager(),"alarm clock");
+
+        picker.addOnPositiveButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (picker.getHour() > 12){
+
+                    binding.time.setText(
+                            String.format("%02d",(picker.getHour()-12))+" : "+String.format("%02d",picker.getMinute())+" PM"
+                    );
+
+                }else {
+
+                    binding.time.setText(picker.getHour()+" : " + picker.getMinute() + " AM");
+
+                }
+
+                calendar = Calendar.getInstance();
+                calendar.set(Calendar.HOUR_OF_DAY,picker.getHour());
+                calendar.set(Calendar.MINUTE,picker.getMinute());
+                calendar.set(Calendar.SECOND,0);
+                calendar.set(Calendar.MILLISECOND,0);
+
+            }
+        });
+
+
+    }
+
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = "Alarm Clock";
+            String description = "This is project made for The MAD lab";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("alarm_clock",name,importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+        }
+
+
+    }
+
+}
